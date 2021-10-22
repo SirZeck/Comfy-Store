a4ccf18b9979acf8fdf6200c62a0d7562e8e9650
Aditya Raj <adithebest.ar@gmail.com>
2021-10-22 14:52:45 +0530

    project complete

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -49,11 +49,26 @@ function displayCartItemCount() {
   cartItemCountDOM.textContent = amount
 }
 
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id != id)
+}
 function increaseAmount(id) {
   let newAmount
   cart = cart.map((cartItem) => {
     if (cartItem.id === id) {
       newAmount = cartItem.amount + 1
+      cartItem = { ...cartItem, amount: newAmount }
+    }
+    return cartItem
+  })
+  return newAmount
+}
+
+function decreaseAmount(id) {
+  let newAmount
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1
       cartItem = { ...cartItem, amount: newAmount }
     }
     return cartItem
@@ -74,7 +89,42 @@ function displayCartItemsDOM() {
   })
 }
 
-function setupCartFunctionality() {}
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener('click', function (e) {
+    const element = e.target
+    const parent = e.target.parentElement
+    const id = e.target.dataset.id
+    const parentID = e.target.parentElement.dataset.id
+
+    //remove
+    if (element.classList.contains('cart-item-remove-btn')) {
+      removeItem(id)
+      parent.parentElement.remove()
+      element.parentElement.parentElement.remove()
+    }
+
+    //increase
+    if (parent.classList.contains('cart-item-increase-btn')) {
+      const newAmount = increaseAmount(parentID)
+      parent.nextElementSibling.textContent = newAmount
+    }
+
+    //decrease
+    if (parent.classList.contains('cart-item-decrease-btn')) {
+      const newAmount = decreaseAmount(parentID)
+      if (newAmount === 0) {
+        removeItem(parentID)
+        parent.parentElement.parentElement.remove()
+      } else {
+        parent.previousElementSibling.textContent = newAmount
+      }
+    }
+
+    displayCartItemCount()
+    displayCartTotal()
+    setStorageItem('cart', cart)
+  })
+}
 
 const init = () => {
   //display amount of cart items
